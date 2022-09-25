d7fde485ff2b22579aea9dc5c9315a92bc0abd0c
Henry Ji <jeep.jipu@gmail.com>
2022-09-25 18:55:27 +0800

    joke: 更新ui

--- apps/joke/static/joke/js/app.js
+++ apps/joke/static/joke/js/app.js
@@ -1,4 +1,7 @@
 let masonryGrid = document.querySelector('#masonryGrid');
-var masonry = new Masonry(masonryGrid, {
-    itemSelector: '.masonry-item'
-});
+window.onload = () => {
+    console.log("ok...")
+    var masonry = new Masonry(masonryGrid, {
+        itemSelector: '.masonry-item',
+    });
+}
