911535a9004448513f8c1034fefaf08308bb14aa
Henry Ji <jeep.jipu@gmail.com>
2022-09-25 18:10:44 +0800

    joke: 更新页面内容数量

--- apps/joke/views.py
+++ apps/joke/views.py
@@ -8,7 +8,7 @@ def index(request):
     photo_list = [ {"type": "photo", "content" : content} for content in list(Photo.objects.all())]
     text_list = [ {"type": "text", "content" : content} for content in list(Text.objects.all())]
     try:
-        joke_list = random.sample(photo_list+text_list, 50)
+        joke_list = random.sample(photo_list+text_list, 20)
     except ValueError:
         joke_list = photo_list + text_list
         random.shuffle(joke_list)
