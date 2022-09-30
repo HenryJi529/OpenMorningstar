04114135bdc8c13863d700303d5b052a5136937f
Henry Ji <jeep.jipu@gmail.com>
2022-09-30 22:49:21 +0800

    poll: 更新模型

--- apps/poll/models.py
+++ apps/poll/models.py
@@ -27,8 +27,7 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(
-        Question, verbose_name="问题", on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, verbose_name="问题", on_delete=models.CASCADE)
     choice_text = models.CharField("选项", max_length=200)
     votes = models.IntegerField("票数", default=0)
 
