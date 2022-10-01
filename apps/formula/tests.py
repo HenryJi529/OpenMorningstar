4007e087c85e3bd10a1076204750f1e5d74d477b
Henry Ji <jeep.jipu@gmail.com>
2022-10-01 12:57:52 +0800

    formula: 更新测试

--- apps/formula/tests.py
+++ apps/formula/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.test import Client
 
-# Create your tests here.
+
+class ViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_index(self):
+        response = self.client.get(reverse('formula:index'))
+        self.assertContains(response, "LaTex公式转换")
