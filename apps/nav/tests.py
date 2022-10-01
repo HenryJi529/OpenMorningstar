57a8ea1877daf7047744374899ae7949c4bd2f46
Henry Ji <jeep.jipu@gmail.com>
2022-10-01 12:58:07 +0800

    nav: 更新测试

---
+++ apps/nav/tests.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.test import Client
+
+from Morningstar.models import User
+
+class ViewTestCase(TestCase):
+    def setUp(self):
+        self.superuser = User.objects.create_superuser(username='admin', email='admin@morningstar529.com', password='admin')
+        self.client = Client()
+
+    def test_index_normal(self):
+        response = self.client.get(reverse('nav:index'))
+        self.assertContains(response, "晨星导航")
+
+    def test_index_admin(self):
+        self.client._login(self.superuser)
+        response = self.client.get(reverse('nav:index'))
+        self.assertContains(response, "项目管理")
+        self.assertContains(response, "UserName")
