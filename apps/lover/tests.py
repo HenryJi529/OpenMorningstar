3383181a8fa9f90d4ee4c2ce5c55e5837186ad5d
Henry Ji <jeep.jipu@gmail.com>
2022-10-01 12:57:17 +0800

    lover: 更新测试

--- apps/lover/tests.py
+++ apps/lover/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+import ast
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+from django.test import Client
+
+
+class ViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_index(self):
+        response = self.client.get(reverse('lover:index'))
+        self.assertContains(response, "谨以此页献给我的爱人FQL~")
+
+    def test_show(self):
+        response = self.client.get(reverse('lover:show'))
+        self.assertTrue(response.status_code, 200)
+
+    def test_api(self):
+        response = self.client.get(reverse('lover:api'))
+        self.assertEqual(ast.literal_eval(response.content.decode())["status"], "success")
+
+    def test_origin(self):
+        response = self.client.get(reverse('lover:origin'))
+        self.assertTrue(response.status_code, 302)
--- apps/lover/views.py
+++ apps/lover/views.py
@@ -1,22 +1,20 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse, JsonResponse
 import os
 import io
 from PIL import Image
 import numpy as np
 import matplotlib
-from Morningstar.settings.common import MEDIA_ROOT
 from random import randint
 
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.http import require_GET
+
+from Morningstar.settings.common import MEDIA_ROOT
 
 def __get_random_image():
 	def get_image_list(image_dir):
 		items = os.listdir(image_dir)
-		image_list = []
-		for item in items:
-			ext = item.split('.')[1]
-			if ext in ["png", "jpg", "jpeg"]:
-				image_list.append(os.path.join(image_dir, item))
+		image_list = [os.path.join(image_dir, item) for item in items if item.split('.')[1] in ["png", "jpg", "jpeg"]]
 		return image_list
 
 	image_dir = os.path.join(MEDIA_ROOT, 'lover')
@@ -34,41 +32,41 @@ def show(request):
     return HttpResponse(stream.getvalue(),"image/png")
 
 
+@require_GET
 def api(request):
-	if request.method == "GET":
-		# 读取客户端参数
-		is_mobile = request.GET.get('isMobile')
+	# 读取客户端参数
+	is_mobile = request.GET.get('isMobile')
 
-		# 生成适配客户端的图片
-		# 适配图片：1280x1706 600x800 3456x4608 1080x1440
-		origin_image = __get_random_image()
+	# 生成适配客户端的图片
+	# 适配图片：1280x1706 600x800 3456x4608 1080x1440
+	origin_image = __get_random_image()
 
-		# if is_mobile == 'true':
-		# 	image_width = 768
-		# 	image_height = 1024
-		# else:
-		# 	image_width = 384
-		# 	image_height = 512
-		image_width = 384
-		image_height = 512
-		new_image = origin_image.resize((image_width,image_height))  # (width, height)
+	# if is_mobile == 'true':
+	# 	image_width = 768
+	# 	image_height = 1024
+	# else:
+	# 	image_width = 384
+	# 	image_height = 512
+	image_width = 384
+	image_height = 512
+	new_image = origin_image.resize((image_width,image_height))  # (width, height)
 
-		# 数据处理
-		array = np.asarray(new_image)  # (height,width,3)
-		result = {
-			"status": "success",
-			"fills": [
-			]
-		}
+	# 数据处理
+	array = np.asarray(new_image)  # (height,width,3)
+	result = {
+		"status": "success",
+		"fills": [
+		]
+	}
 
-		for i in range(0,image_height-4+1,4):
-			for j in range(0,image_width-3+1,3):
-				r = array[i+1,j+1,0]/2 + array[i+2,j+1,0]/2
-				g = array[i+1,j+1,1]/2 + array[i+2,j+1,1]/2
-				b = array[i+1,j+1,2]/2 + array[i+2,j+1,2]/2
-				rgb = matplotlib.colors.rgb2hex([r/255,g/255,b/255])
-				result["fills"].append(rgb)
-		return JsonResponse(result)
+	for i in range(0,image_height-4+1,4):
+		for j in range(0,image_width-3+1,3):
+			r = array[i+1,j+1,0]/2 + array[i+2,j+1,0]/2
+			g = array[i+1,j+1,1]/2 + array[i+2,j+1,1]/2
+			b = array[i+1,j+1,2]/2 + array[i+2,j+1,2]/2
+			rgb = matplotlib.colors.rgb2hex([r/255,g/255,b/255])
+			result["fills"].append(rgb)
+	return JsonResponse(result)
 
 
 def index(request):
