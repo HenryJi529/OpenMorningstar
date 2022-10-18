6657e697f176d30cae21ded74e507b50e651d085
Henry Ji <jeep.jipu@gmail.com>
2022-10-18 21:00:05 +0800

    构造邮件模版

--- Morningstar/lib/mail.py
+++ Morningstar/lib/mail.py
@@ -1,7 +1,18 @@
 from django.core.mail import send_mail
 
 from Morningstar.settings.common import EMAIL_HOST_USER
+from Morningstar.settings.common import EMAIL_TEMPLATE_TEXT
 
 
 def send_mail_from_host(subject, message, email_list):
-    send_mail(subject, message, EMAIL_HOST_USER, email_list)
+    response = send_mail(subject, message, EMAIL_HOST_USER, email_list)
+    return response
+
+
+def send_mail_from_host_by_template(email_list, template, template_param_list):
+    subject = EMAIL_TEMPLATE_TEXT[template]['subject']
+    message = EMAIL_TEMPLATE_TEXT[template]['message'](*template_param_list)
+    response = send_mail(subject, message, EMAIL_HOST_USER, email_list)
+    return response
+
+
