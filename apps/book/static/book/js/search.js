f65601b644600bf88499b5f55969499dca07fc72
Henry Ji <jeep.jipu@gmail.com>
2022-10-18 13:52:55 +0800

    book: 更新UI

--- apps/book/static/book/js/search.js
+++ apps/book/static/book/js/search.js
@@ -16,19 +16,33 @@ function findMatches(wordToMatch, bookList) {
 
 function displayMatches() {
 	const matchArray = findMatches(this.value, bookList);
-	const html = matchArray.map(book => {
+	const isMobile = /Android|iPhone/i.test(navigator.userAgent) ? true : false;
+	const maxNum = isMobile ? 8 : 10;
+	const html = matchArray.map((book, index, array) => {
 		const regex = new RegExp(this.value, 'gi');
 		const bookName = book.book_name.replace(regex, `<span class="text-orange-500">${this.value}</span>`);
 		const bookAuthor = book.author_name.replace(regex, `<span class="text-orange-500">${this.value.toUpperCase()}</span>`);
-		return `
-	<li class="flex justify-between items-center p-4 bg-gradient-to-r from-white to-slate-200">
-		<span class="font-bold">
-			<a href="${book.uri}"><i class="fa-solid fa-download"> </i></a>
-			${bookName}
-		</span>
-		<span class="font-bold">${bookAuthor}</span>
-	</li>
-	`;
+		const normal = `<li class="flex justify-between items-center p-4 bg-gradient-to-r from-white to-slate-200 text-sm md:text-base">
+			<span class="font-bold">
+				<a href="${book.uri}"><i class="fa-solid fa-download"> </i></a>
+				${bookName}
+			</span>
+			<span class="font-bold">${bookAuthor}</span>
+		</li>`
+		const abnormal = `<li class="flex justify-center items-center p-4 bg-gradient-to-r from-white to-slate-200 text-sm md:text-base">
+			<span class="font-bold">. . .</span>
+		</li>`;
+		if (array.length <= maxNum) {
+			return normal;
+		} else {
+			if (index === maxNum / 2 + 1) {
+				return abnormal;
+			} else if (index < maxNum / 2 || index > array.length - maxNum / 2 - 1) {
+				return normal;
+			} else {
+				return null;
+			}
+		}
 	}).join('');
 	suggestionList.innerHTML = html;
 }
