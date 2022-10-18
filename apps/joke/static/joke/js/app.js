b08bf6f2d14d67a60bf1dd3cc2f798f9f7ec367e
Henry Ji <jeep.jipu@gmail.com>
2022-10-18 20:55:40 +0800

    joke: 删除注释

--- apps/joke/static/joke/js/app.js
+++ apps/joke/static/joke/js/app.js
@@ -1,6 +1,6 @@
 let masonryGrid = document.querySelector('#masonryGrid');
 window.onload = () => {
-    console.log("ok...")
+    // console.log("ok...")
     var masonry = new Masonry(masonryGrid, {
         itemSelector: '.masonry-item',
     });
