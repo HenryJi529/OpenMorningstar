b6925ef400159f3fccf9ced1dffc35d723b0f9d7
Henry Ji <jeep.jipu@gmail.com>
2022-11-29 14:22:54 +0800

    joke: 添加进度条

--- apps/joke/static/joke/js/app.js
+++ apps/joke/static/joke/js/app.js
@@ -1,6 +1,8 @@
 let masonryGrid = document.querySelector('#masonryGrid');
+let progressBarContainer = document.querySelector('#progressBarContainer');
 window.onload = () => {
-    // console.log("ok...")
+    // console.log("ok...window is onload...");
+    progressBarContainer.classList.add('hidden');
     var masonry = new Masonry(masonryGrid, {
         itemSelector: '.masonry-item',
     });
