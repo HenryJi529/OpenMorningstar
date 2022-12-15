e9dbc3ed245aabcd9e466e4ca5ee3796f443718e
Henry Ji <jeep.jipu@gmail.com>
2022-12-16 00:19:40 +0800

    更新测试

--- Morningstar/tests/test_views.py
+++ Morningstar/tests/test_views.py
@@ -13,8 +13,7 @@ class ShortCutViewTest(TestCase):
         SLUGS = [
             "issue", "auto", "src", "src-mirror", "host", "vercel", "domain", "namecheap", "license", "coverage", "task", "resume", "wechatpp",
             "qiniu", "lanzou",
-            "gist", "html", "css", "js", "bash", "sass",
-            "sgs", "cook", "news"
+            "gist", "sgs", "cook", "news"
         ]
         for slug in SLUGS:
             response = self.client.get(reverse("shortcut", args=[slug]))
