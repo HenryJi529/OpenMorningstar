8466d3d4660f15e3d5307c04472bf9b2bbc8ec72
Henry Ji <jeep.jipu@gmail.com>
2023-02-05 10:11:22 +0800

    更新插件

---
+++ extensions/popup/js/main.js
@@ -0,0 +1,21 @@
+const endpoint = "https://morningstar369.com/joke/api/?n=1"
+// const endpoint = "http://127.0.0.1:8000/joke/api/?n=1"
+const maxHeight = 600
+
+fetch(endpoint)
+    .then(response => response.json())
+    .then(data => data.links[0])
+    .then(link => {
+        const imgElement = document.createElement('img')
+        imgElement.src = link
+        imgElement.onload = () => {
+            if (imgElement.naturalHeight < maxHeight) {
+                document.body.style.height = `${imgElement.naturalHeight}px`;
+                document.body.style.width = `${imgElement.naturalWidth}px`;
+            } else {
+                document.body.style.height = `${maxHeight}px`;
+                document.body.style.width = `${600 * imgElement.naturalWidth / imgElement.naturalHeight}px`;
+            }
+            document.body.appendChild(imgElement)
+        }
+    })
