67068222dc2f71b93187bba2911156bf332699ba
Henry Ji <jeep.jipu@gmail.com>
2023-05-11 12:08:57 +0800

    更新UI

--- apps/blog/static/blog/js/toggleTheme.js
+++ apps/blog/static/blog/js/toggleTheme.js
@@ -4,16 +4,16 @@ let themeIcon = toggleThemeButton.querySelector("#themeIcon");
 let html = document.querySelector("html");
 
 // 使用localStorage-slim替代原生的localStorage
-if(window.ls.get("darkMode") === "true"){
+if (window.ls.get("darkMode") === "true") {
     html.setAttribute("data-theme", "dark");
     html.classList.add("dark");
     themeIcon.classList.add("fa-moon");
 }
-else if(window.ls.get("darkMode") === "false"){
+else if (window.ls.get("darkMode") === "false") {
     html.setAttribute("data-theme", "light");
     html.classList.add("light");
     themeIcon.classList.add("fa-sun");
-}else{
+} else {
     let currentHours = new Date().getHours();
     if (currentHours >= 18 || currentHours <= 4) {
         html.setAttribute("data-theme", "dark");
@@ -33,13 +33,13 @@ toggleThemeButton.addEventListener("click", () => {
         html.classList.remove("light");
         themeIcon.classList.remove("fa-sun");
         themeIcon.classList.add("fa-moon");
-        window.ls.set("darkMode", "true",{ ttl: 60*30 });
+        window.ls.set("darkMode", "true", { ttl: 60 * 30 });
     } else {
         html.setAttribute("data-theme", "light");
         html.classList.add("light");
         html.classList.remove("dark");
         themeIcon.classList.remove("fa-moon");
         themeIcon.classList.add("fa-sun");
-        window.ls.set("darkMode", "false",{ ttl: 60*30 });
+        window.ls.set("darkMode", "false", { ttl: 60 * 30 });
     }
 })
