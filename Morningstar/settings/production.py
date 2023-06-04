63f83798a49792d70e46bf52c8be4f877de8c4bc
Henry Ji <jeep.jipu@gmail.com>
2023-06-04 22:00:58 +0800

    更换IP

--- Morningstar/settings/production.py
+++ Morningstar/settings/production.py
@@ -3,35 +3,35 @@ import os
 
 """一系列安全措施"""
 # NOTE: 检测方法: python3 manage.py check --settings=Morningstar.settings.production --deploy
-X_FRAME_OPTIONS = 'SAMEORIGIN'  # NOTE: 这是用来支持同源的iframe 一般用'DENY'
+X_FRAME_OPTIONS = "SAMEORIGIN"  # NOTE: 这是用来支持同源的iframe 一般用'DENY'
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
 
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
-MYSQL_ROOT_PASSWORD = os.environ['MYSQL_ROOT_PASSWORD']
-REDIS_PASSWORD = os.environ['REDIS_PASSWORD']
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+MYSQL_ROOT_PASSWORD = os.environ["MYSQL_ROOT_PASSWORD"]
+REDIS_PASSWORD = os.environ["REDIS_PASSWORD"]
 DEBUG = False
-ALLOWED_HOSTS = ['morningstar369.com', '43.154.147.176']
-CSRF_TRUSTED_ORIGINS = ['https://morningstar369.com']
+ALLOWED_HOSTS = ["morningstar369.com", "43.154.26.218"]
+CSRF_TRUSTED_ORIGINS = ["https://morningstar369.com"]
 
 
 """邮件"""
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'djangodb',
-        'USER': 'root',
-        'PASSWORD': MYSQL_ROOT_PASSWORD,
-        'HOST': 'db1',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "djangodb",
+        "USER": "root",
+        "PASSWORD": MYSQL_ROOT_PASSWORD,
+        "HOST": "db1",
+        "PORT": "3306",
+        "OPTIONS": {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+            "charset": "utf8mb4",
         },
     }
 }
@@ -44,18 +44,15 @@ CACHES = {
         "LOCATION": "redis://redis:6379",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
-            "CONNECTION_POOL_KWARGS": {
-                "max_connections": 1000,
-                "encoding": 'utf-8'
-            },
-            "PASSWORD": REDIS_PASSWORD
-        }
+            "CONNECTION_POOL_KWARGS": {"max_connections": 1000, "encoding": "utf-8"},
+            "PASSWORD": REDIS_PASSWORD,
+        },
     }
 }
 
 """RECAPTCHA-V2"""
-RECAPTCHA_PUBLIC_KEY = os.environ['RECAPTCHA_PUBLIC_KEY']
-RECAPTCHA_PRIVATE_KEY = os.environ['RECAPTCHA_PRIVATE_KEY']
+RECAPTCHA_PUBLIC_KEY = os.environ["RECAPTCHA_PUBLIC_KEY"]
+RECAPTCHA_PRIVATE_KEY = os.environ["RECAPTCHA_PRIVATE_KEY"]
 
 """维护中站点列表"""
 MAINTENANCE_URL_LIST = [
@@ -69,4 +66,3 @@ COMPRESS_ENABLED = True
 CORS_ALLOWED_ORIGINS = [
     "https://morningstar369.com",
 ]
-
