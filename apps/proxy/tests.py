5852d7e7f1cbf8f74ec6a107e5a290e46a88b1a9
Henry Ji <jeep.jipu@gmail.com>
2023-06-07 11:48:44 +0800

    proxy: 更新测试

--- apps/proxy/tests.py
+++ apps/proxy/tests.py
@@ -6,10 +6,6 @@ from django.test import Client
 
 
 class ViewTestCase(TestCase):
-    def test_index(self):
-        response = self.client.get(reverse("proxy:index"))
-        self.assertTrue("vmess://" in base64.b64decode(response.content).decode())
-
     def test_config(self):
         response = self.client.get(reverse("proxy:config"))
         self.assertEqual(response.status_code, 302)
