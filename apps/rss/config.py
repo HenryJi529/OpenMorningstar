6c7584a1d9652717ac7ab1d21b07a9a82ac98a3a
Henry Ji <jeep.jipu@gmail.com>
2023-06-07 10:27:26 +0800

    rss: 更新regex

--- apps/rss/config.py
+++ apps/rss/config.py
@@ -18,7 +18,7 @@ SITES = [
         "description": "河海大学研究生学院通知公告, 通过晨星小站抓取",
         "language": "zh-Hans",
         "feed": {
-            "pattern": r"<td align=\"left\"><a href='(/\d{4}/\d{4}/\w*/page\.htm)' target='_blank' title='([^(font)]*)'>.*</a></td>\s*<td align=\"left\" width=\"30px\"><div style=\"white-space:nowrap\">(\d{4}-\d{2}-\d{2})</div></td>",
+            "pattern": r"<li class=\"news n\d{1,2} clearfix\">\s*<span class=\"news_title\"><a href=\'(\/\d{4}\/\d{4}\/c3624a\w{6}\/page\.htm)\' target=\'_blank\' title=\'(.*)\'>(?:\2|<font style=\'color:#\w{6};\'>\2<\/font>)<\/a><\/span>\s*<span class=\"news_meta\">(\d{4}-\d{2}-\d{2})<\/span>\s*<\/li>",
             "link_index": 1,
             "title_index": 2,
             "pubDate_index": 3,
