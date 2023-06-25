225060210e24bfe24f29ea6189212a3726799fdc
Henry Ji <jeep.jipu@gmail.com>
2023-06-25 10:11:05 +0800

    book: 更新视图

--- apps/book/views.py
+++ apps/book/views.py
@@ -5,8 +5,10 @@ from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 from django.shortcuts import render, reverse
 from django.views import View
 
+from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from Morningstar.views.base import fix_fetched_post
 from .models import Book
