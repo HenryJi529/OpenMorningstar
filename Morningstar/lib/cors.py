866808a6da7cc003d70557096c3d146d3a14e52f
Henry Ji <jeep.jipu@gmail.com>
2023-07-02 11:21:50 +0800

    更新cros相关函数

--- Morningstar/lib/cors.py
+++ Morningstar/lib/cors.py
@@ -1,7 +1,7 @@
 def add_cors_header(func):
     def wrapper(request, *args, **kwargs):
         response = func(request, *args, **kwargs)
-        response['Access-Control-Allow-Origin'] = '*'
+        response["Access-Control-Allow-Origin"] = "*"
         return response
 
     return wrapper
