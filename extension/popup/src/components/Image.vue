afd17aa1433c2b5acccfba8f3026f10b9ed17675
Henry Ji <jeep.jipu@gmail.com>
2023-07-03 10:51:54 +0800

    ext: 更新UI

--- extension/popup/src/components/Image.vue
+++ extension/popup/src/components/Image.vue
@@ -6,6 +6,7 @@ axios.defaults.baseURL = process.env.BASE_URL
 const endpoint = "/joke/images/random?n=1"
 
 const imageUrl = ref("")
+const showLoading = ref(true)
 
 onMounted(async () => {
     const response = await axios.get(endpoint);
@@ -15,7 +16,12 @@ onMounted(async () => {
 </script>
 
 <template>
-  <main class="w-96">
-    <img :src="imageUrl" v-if="imageUrl" class="w-full">
+  <main class="w-96 relative">
+    <img :src="imageUrl" v-if="imageUrl" class="w-full" @load="showLoading = !showLoading">
+    <div v-if="showLoading" class="absolute top-0 z-10">
+      <div class="h-96 w-96 flex justify-center items-center">
+        <span class="loading loading-dots loading-lg"></span>
+      </div>
+    </div>
   </main>
 </template>
