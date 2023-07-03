dd4fc645ed59021e1cdf9a0ead19949d2b83f7c8
Henry Ji <jeep.jipu@gmail.com>
2023-07-03 10:23:57 +0800

    ext: 更新endpoint

--- extension/popup/src/components/Image.vue
+++ extension/popup/src/components/Image.vue
@@ -3,13 +3,13 @@ import { onMounted, ref } from "vue";
 
 import axios from "axios";
 axios.defaults.baseURL = process.env.BASE_URL
-const endpoint = "/joke/images/?n=1"
+const endpoint = "/joke/images/random?n=1"
 
 const imageUrl = ref("")
 
 onMounted(async () => {
     const response = await axios.get(endpoint);
-    imageUrl.value = response.data.status==="ok" ? response.data.links[0] : "";
+    imageUrl.value = response.data.status==="ok" ? response.data.objects[0].link : "";
 })
 
 </script>
