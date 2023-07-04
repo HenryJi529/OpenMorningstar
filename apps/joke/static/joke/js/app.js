e19ce2771e0852337ed6d476206d008d7eb0c94b
Henry Ji <jeep.jipu@gmail.com>
2023-07-04 08:06:49 +0800

    joke: 删除static

--- apps/joke/static/joke/js/app.js
+++
@@ -1,9 +0,0 @@
-let masonryGrid = document.querySelector('#masonryGrid');
-let progressBarContainer = document.querySelector('#progressBarContainer');
-window.onload = () => {
-    // console.log("ok...window is onload...");
-    progressBarContainer.classList.add('hidden');
-    var masonry = new Masonry(masonryGrid, {
-        itemSelector: '.masonry-item',
-    });
-}
