4adadd08f71cea9a9eee71504dc73a8b58161854
Henry Ji <jeep.jipu@gmail.com>
2023-07-05 21:42:28 +0800

    quiz: 删除无用的导入

--- apps/quiz/frontend/vite.config.js
+++ apps/quiz/frontend/vite.config.js
@@ -2,7 +2,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import axios from 'axios';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode, ssrBuild }) => {
