477a1470a15e4d95725b93b802f08f4267a288b1
Henry Ji <jeep.jipu@gmail.com>
2023-07-25 10:58:20 +0800

    blog: 设置目录格式

--- apps/blog/templatetags/blog_extras.py
+++ apps/blog/templatetags/blog_extras.py
@@ -7,53 +7,62 @@ from ..models import Post, Category, Tag
 register = template.Library()
 
 
-@register.inclusion_tag('blog/inclusions/_recent_posts.html', takes_context=True)
+@register.inclusion_tag("blog/inclusions/_recent_posts.html", takes_context=True)
 def show_recent_posts(context, num=5):
     return {
-        'recent_post_list': Post.objects.all()[:num],
+        "recent_post_list": Post.objects.all()[:num],
     }
 
 
-@register.inclusion_tag('blog/inclusions/_archives.html', takes_context=True)
+@register.inclusion_tag("blog/inclusions/_archives.html", takes_context=True)
 def show_archives(context):
     return {
-        'date_list': Post.objects.dates('created', 'month', order='DESC'),
+        "date_list": Post.objects.dates("created", "month", order="DESC"),
     }
 
 
-@register.inclusion_tag('blog/inclusions/_categories.html', takes_context=True)
+@register.inclusion_tag("blog/inclusions/_categories.html", takes_context=True)
 def show_categories(context):
-    category_list = Category.objects.annotate(
-        num_posts=Count('post')).filter(num_posts__gt=0)
+    category_list = Category.objects.annotate(num_posts=Count("post")).filter(
+        num_posts__gt=0
+    )
     return {
-        'category_list': category_list,
+        "category_list": category_list,
     }
 
 
-@register.inclusion_tag('blog/inclusions/_tags.html', takes_context=True)
+@register.inclusion_tag("blog/inclusions/_tags.html", takes_context=True)
 def show_tags(context):
-    tag_list = Tag.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0).order_by('-num_posts')
+    tag_list = (
+        Tag.objects.annotate(num_posts=Count("post"))
+        .filter(num_posts__gt=0)
+        .order_by("-num_posts")
+    )
     try:
         maxNum = max([tag.num_posts for tag in tag_list])
         minNum = min([tag.num_posts for tag in tag_list])
         levelNum = 5
         for tag in tag_list:
-            tag.color_level = int( (tag.num_posts - minNum) / (maxNum - minNum) * (levelNum-1) ) + 1
+            tag.color_level = (
+                int((tag.num_posts - minNum) / (maxNum - minNum) * (levelNum - 1)) + 1
+            )
     except (ValueError, ZeroDivisionError):
         for tag in tag_list:
             tag.color_level = 2
     return {
-        'tag_list': tag_list,
+        "tag_list": tag_list,
     }
 
-@register.inclusion_tag('blog/inclusions/_hot_posts.html', takes_context=True)
-def show_hot_posts(context,num=5):
-    hot_post_list = Post.objects.order_by('-views')[:num]
+
+@register.inclusion_tag("blog/inclusions/_hot_posts.html", takes_context=True)
+def show_hot_posts(context, num=5):
+    hot_post_list = Post.objects.order_by("-views")[:num]
     return {
-        'hot_post_list': hot_post_list,
+        "hot_post_list": hot_post_list,
     }
 
-@register.inclusion_tag('blog/inclusions/_other_links.html', takes_context=False)
+
+@register.inclusion_tag("blog/inclusions/_other_links.html", takes_context=False)
 def show_other_links():
     pass
 
@@ -61,6 +70,16 @@ def show_other_links():
 @register.simple_tag(takes_context=False)
 def show_lover():
     try:
-        return VisiableSetting.objects.get(key='lover').value
+        return VisiableSetting.objects.get(key="lover").value
     except VisiableSetting.DoesNotExist:
+        return True
+
+
+@register.simple_tag(takes_context=False)
+def toc_not_null(toc: str):
+    if toc.replace(" ", "").replace(
+        "\n", ""
+    ) == '<div class="toc"><ul></ul></div>'.replace(" ", "").replace("\n", ""):
+        return False
+    else:
         return True
