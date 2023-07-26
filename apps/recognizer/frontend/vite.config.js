042dab3fc07848e4792d76939af06c8fbc9e8ba1
Henry Ji <jeep.jipu@gmail.com>
2023-07-26 19:14:34 +0800

    recognizer: 连接前后端

--- apps/recognizer/frontend/vite.config.js
+++ apps/recognizer/frontend/vite.config.js
@@ -3,14 +3,26 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+export default defineConfig(({ command, mode, ssrBuild }) => {
+
+  const isServe = command === 'serve';
+  const isDev = mode === 'development';
+
+  // 根据不同命令设置不同的基础URL
+  const baseURL = isServe ? "http://localhost:8000/api/recognizer/" : "https://morningstar369.com/api/recognizer/";
+
+  return {
+    base: '/recognizer/',
+    plugins: [vue()],
+    define: {
+      'process.env.BASE_URL': JSON.stringify(baseURL)
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
   }
 })
