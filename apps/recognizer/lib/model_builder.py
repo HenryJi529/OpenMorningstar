f7577305a9f2860d1c90b3b8ea43a76e18632e7e
Henry Ji <jeep.jipu@gmail.com>
2023-08-03 16:49:35 +0800

    新增LinearRegressionModelV2

--- apps/recognizer/lib/model_builder.py
+++ apps/recognizer/lib/model_builder.py
@@ -29,6 +29,15 @@ class LinearRegressionModel(nn.Module):
         return self.weights * x + self.bias
 
 
+class LinearRegressionModelV2(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear_layer = nn.Linear(in_features=1, out_features=1)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.linear_layer(x)
+
+
 class TinyVGG(nn.Module):
     """Creates the TinyVGG architecture.
 
