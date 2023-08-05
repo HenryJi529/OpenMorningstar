8475d4b00885589ba54cbef41b831f6419fdc7a7
Henry Ji <jeep.jipu@gmail.com>
2023-08-05 19:54:21 +0800

    实现 图片数据集下载 函数

--- apps/recognizer/lib/data_processor.py
+++ apps/recognizer/lib/data_processor.py
@@ -1,6 +1,8 @@
 import json
 import re
+import requests
 import os
+import zipfile
 import math
 from pathlib import Path
 from typing import TypeVar, Tuple, Dict, List, Union
@@ -173,6 +175,55 @@ def create_dataloaders(
     )
 
     return train_dataloader, val_dataloader, test_dataloader, categories
+
+
+def download_image_dataset(
+    source: str, destination: str, remove_source: bool = True
+) -> Path:
+    """Downloads a zipped dataset from source and unzips to destination.
+
+    Args:
+        source (str): A link to a zipped file containing data.
+        destination (str): A target directory to unzip data to.
+        remove_source (bool): Whether to remove the source after downloading and extracting.
+
+    Returns:
+        pathlib.Path to downloaded data.
+
+    Example usage:
+        download_data(
+                        source="https://github.com/mrdbourke/pytorch-deep-learning/raw/main/data/pizza_steak_sushi.zip",
+                        destination="pizza_steak_sushi"
+                    )
+    """
+    # Setup path to data folder
+    data_path = Path("data/")
+    image_path = data_path / destination
+
+    # If the image folder doesn't exist, download it and prepare it...
+    if image_path.is_dir():
+        print(f"[INFO] {image_path} directory exists, skipping download.")
+    else:
+        print(f"[INFO] Did not find {image_path} directory, creating one...")
+        image_path.mkdir(parents=True, exist_ok=True)
+
+        # Download pizza, steak, sushi data
+        target_file = Path(source).name
+        with open(data_path / target_file, "wb") as f:
+            request = requests.get(source)
+            print(f"[INFO] Downloading {target_file} from {source}...")
+            f.write(request.content)
+
+        # Unzip pizza, steak, sushi data
+        with zipfile.ZipFile(data_path / target_file, "r") as zip_ref:
+            print(f"[INFO] Unzipping {target_file} data...")
+            zip_ref.extractall(image_path)
+
+        # Remove .zip file
+        if remove_source:
+            os.remove(data_path / target_file)
+
+    return image_path
 
 
 class CustomImageFolder(Dataset):
