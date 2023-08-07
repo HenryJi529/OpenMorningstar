4243c540c49d48fa0ceb79624234500705e69494
Henry Ji <jeep.jipu@gmail.com>
2023-08-07 12:25:48 +0800

    新增数据集拆分函数

--- apps/recognizer/lib/data_processor.py
+++ apps/recognizer/lib/data_processor.py
@@ -9,12 +9,13 @@ from typing import TypeVar, Tuple, Dict, List, Union
 
 from PIL import Image
 
-from torch import Tensor
+from torch import Tensor, manual_seed
 from torch.utils.data import Dataset
 from torchvision import datasets, transforms
 from torchvision.datasets.vision import VisionDataset
 from torch.utils.data import DataLoader
 from torch.utils.data import random_split
+from torchvision import datasets
 
 
 BATCH_SIZE = 32
@@ -175,6 +176,35 @@ def create_dataloaders(
     )
 
     return train_dataloader, val_dataloader, test_dataloader, categories
+
+
+def split_dataset(dataset: datasets, split_size: float = 0.2, seed: int = 42):
+    """Randomly splits a given dataset into two proportions based on split_size and seed.
+
+    Args:
+        dataset (torchvision.datasets): A PyTorch Dataset, typically one from torchvision.datasets.
+        split_size (float, optional): How much of the dataset should be split?
+            E.g. split_size=0.2 means there will be a 20% split and an 80% split. Defaults to 0.2.
+        seed (int, optional): Seed for random generator. Defaults to 42.
+
+    Returns:
+        tuple: (random_split_1, random_split_2) where random_split_1 is of size split_size*len(dataset) and
+            random_split_2 is of size (1-split_size)*len(dataset).
+    """
+    # Create split lengths based on original dataset length
+    length_1 = int(len(dataset) * split_size)  # desired length
+    length_2 = len(dataset) - length_1  # remaining length
+
+    # Print out info
+    print(
+        f"[INFO] Splitting dataset of length {len(dataset)} into splits of size: {length_1} ({int(split_size*100)}%), {length_2} ({int((1-split_size)*100)}%)"
+    )
+
+    # Create splits with given random seed
+    random_split_1, random_split_2 = random_split(
+        dataset, lengths=[length_1, length_2], generator=manual_seed(seed)
+    )  # set the random seed for reproducible splits
+    return random_split_1, random_split_2
 
 
 def download_image_dataset(
