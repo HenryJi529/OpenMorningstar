00217756d9738745f665be8ea0b2abecfd90bc1e
Henry Ji <jeep.jipu@gmail.com>
2024-04-24 19:41:29 +0800

    ext: 更新接口调用

--- extension/popup/src/components/Image.vue
+++ extension/popup/src/components/Image.vue
@@ -3,7 +3,7 @@ import { onMounted, ref } from "vue";
 
 import axios from "axios";
 axios.defaults.baseURL = process.env.BASE_URL
-const endpoint = "/joke/images/?n=1"
+const endpoint = "/joke/photos/?n=1"
 
 const host = process.env.BASE_URL.split('/').slice(0, 3).join('/')
 const imageUrl = ref("")
