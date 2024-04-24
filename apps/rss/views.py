4320d4aae6e5549228e8e90a7724f570e6454546
Henry Ji <jeep.jipu@gmail.com>
2024-04-24 09:15:53 +0800

    rss: 格式化代码

--- apps/rss/views.py
+++ apps/rss/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.urls import reverse
+from django.http import HttpRequest
 
 from .lib import get_data, get_sources_num, get_title_from_index, get_link_from_index
 
 
-def index(request):
+def index(request: HttpRequest):
     items = [
         {
             "title": get_title_from_index(index),
@@ -13,9 +14,21 @@ def index(request):
         }
         for index in range(get_sources_num())
     ]
-    return render(request, "rss/index.html", locals())
+    return render(request, "rss/index.html", {"items": items})
 
 
 def get_xml(request, pk):
     (title, link, description, language, items, lastBuildDate) = get_data(pk - 1)
-    return render(request, "rss/base.xml", locals(), content_type="text/xml")
+    return render(
+        request,
+        "rss/base.xml",
+        {
+            "title": title,
+            "link": link,
+            "description": description,
+            "language": language,
+            "items": items,
+            "lastBuildDate": lastBuildDate,
+        },
+        content_type="text/xml",
+    )
