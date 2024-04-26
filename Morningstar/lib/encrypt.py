d5d7409e69f7cc3847f39e3618bd721336b44933
Henry Ji <jeep.jipu@gmail.com>
2024-04-26 16:04:52 +0800

    base: 更新md5(string: str)

--- Morningstar/lib/encrypt.py
+++ Morningstar/lib/encrypt.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-import uuid
 import hashlib
 import os
 
 
-def md5(string):
-    """ MD5加密 """
-    if os.environ.get('DJANGO_SETTINGS_MODULE','Morningstar.settings.dev') == 'Morningstar.settings.production':
+def md5(string: str):
+    """MD5加密"""
+    if (
+        os.environ.get("DJANGO_SETTINGS_MODULE", "Morningstar.settings.dev")
+        == "Morningstar.settings.production"
+    ):
         from Morningstar.settings import production as settings
+
         SECRET_KEY = settings.SECRET_KEY
     else:
         from Morningstar.settings import dev as settings
+
         SECRET_KEY = settings.SECRET_KEY
 
-    hash_object = hashlib.md5(SECRET_KEY.encode('utf-8'))
-    hash_object.update(string.encode('utf-8'))
+    hash_object = hashlib.md5(SECRET_KEY.encode("utf-8"))
+    hash_object.update(string.encode("utf-8"))
     return hash_object.hexdigest()
-
-
-def uid(string):
-    data = "{}-{}".format(str(uuid.uuid4()), string)
-    return md5(data)
